f630f074c135057492528b90fdc14aaff9e77c9a
Simeon Lisovski <slisovsk@pgeo03m015.dmawi.de>
2021-02-24 10:07:23 +0100

    test

--- code/dataHandling.R
+++ code/dataHandling.R
@@ -16,3 +16,6 @@ brk <- brick(m10path, varname = "u10", level = 1)[[i]]
 wnd  <- extract(tmpBrick[[1:2]], ind[,2:3])
 dir  <- atan2(wnd[,1], wnd[,2]) * (180/pi)
 spd  <- sqrt(wnd[,1]^2 + wnd[,2]^2)
+
+
+test <- 1+10
