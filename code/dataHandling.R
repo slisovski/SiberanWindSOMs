e02b01795450a9b6aeaf436bc61e9a2b0aa0ee3b
Simeon Lisovski <simeon.lisovski@vogelwarte.ch>
2021-02-24 09:57:03 +0100

    codeUpload

---
+++ code/dataHandling.R
@@ -0,0 +1,18 @@
+library(raster)
+library(sf)
+library(ncdf4)
+
+
+pathFile <- "..."
+
+nf <- nc_open(pathFile)
+tms    <- as.POSIXct(nf$var[[1]]$dim[[4]]$vals*60*60, "1900-01-01", tz = "GMT")
+nc_close(nf)
+
+
+brk <- brick(m10path, varname = "u10", level = 1)[[i]]
+
+
+wnd  <- extract(tmpBrick[[1:2]], ind[,2:3])
+dir  <- atan2(wnd[,1], wnd[,2]) * (180/pi)
+spd  <- sqrt(wnd[,1]^2 + wnd[,2]^2)
